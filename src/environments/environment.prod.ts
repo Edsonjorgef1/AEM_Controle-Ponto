81658a07bed04a42618d0f44bbd0766eeedfe126
Edson Jorge Francisco <Edsonjorgef1@gmail.com>
2025-05-08 22:01:01 +0200

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,11 @@
 // src/environments/environment.prod.ts
+declare const process: any; // Add this to avoid TypeScript errors
+
 export const environment = {
   production: true,
-  supabaseUrl: process.env['SUPABASE_URL'] || '',
-  supabaseKey: process.env['SUPABASE_KEY'] || ''
+  // Use process.env variables for production
+  supabaseUrl: process.env['SUPABASE_DATABASE_URL'],
+  supabaseKey: process.env['SUPABASE_ANON_KEY'],
+  supabaseServiceRoleKey: process.env['SUPABASE_SERVICE_ROLE_KEY'],
+  supabaseJwtSecret: process.env['SUPABASE_JWT_SECRET']
 };
