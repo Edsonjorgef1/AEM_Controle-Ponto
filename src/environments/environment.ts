90ec15cc20f2f1e38ccb9369e7313e48b76d676d
Edson Jorge Francisco <Edsonjorgef1@gmail.com>
2025-05-08 21:05:47 +0200

    Update environment.ts
    Removing the superbase setup on local

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  supabaseUrl: 'https://ywmmsjudizjoojufwejo.supabase.co',
-  supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl3bW1zanVkaXpqb29qdWZ3ZWpvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDY1MzUxMjcsImV4cCI6MjA2MjExMTEyN30._ihqLNXF8k7YPM82qtFnsDH80Sz5jHNaxNNhZpVC1hA'
+  supabaseUrl: process.env['SUPABASE_URL'] || '',
+  supabaseKey: process.env['SUPABASE_KEY'] || ''
 };
 
 /*
